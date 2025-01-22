19bfaba3f540c2b2137e9a22865b86c985adb26a
SugamChaudharry <nextgenachieverss@gmail.com>
2025-01-22 06:28:52 +0530

    add jwtlite

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -30,3 +30,31 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
     throw new ApiError(401, error?.message || "Invalid access token");
   }
 });
+export const verifyJWTLite = asyncHandler(async (req, _, next) => {
+  try {
+
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+      
+    if (!token) {
+      next();
+      return;
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
